ebb8ac907df6fe7ddd5ca10d4f577de2913163ae
putrinurulizza <putrinurulizzaa@gmail.com>
2024-06-22 03:31:28 +0700

    fix model

--- app/Models/NaiveBayesModel.php
+++ app/Models/NaiveBayesModel.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Dataset;
 
 class NaiveBayesModel extends Model
 {
@@ -29,9 +28,16 @@ class NaiveBayesModel extends Model
         // Hitung probabilitas prior P(Y=label)
         $total_data = Dataset::count(); // Menghitung jumlah total data
         $count_label = Dataset::where('label', $label)->count(); // Menghitung jumlah data berlabel $label
+
+        if ($count_label == 0) {
+            return 0; // Menghindari division by zero, bisa di-handle sesuai kebutuhan
+        }
+
         $prior = $count_label / $total_data;
 
         // Hitung probabilitas conditional P(Xi=x|Y=label) untuk masing-masing fitur
+        $conditional_prob = [];
+
         $features = [
             'pkh' => $dataset->pkh,
             'jml_tanggungan' => $dataset->jml_tanggungan,
@@ -42,11 +48,17 @@ class NaiveBayesModel extends Model
 
         foreach ($features as $key => $value) {
             $count_feature_label = Dataset::where('label', $label)->where($key, $value)->count(); // Menghitung jumlah data dengan fitur $key bernilai $value dan berlabel $label
-            $conditional_prob[$key] = $count_feature_label / $count_label;
+
+            if ($count_label != 0) {
+                $conditional_prob[$key] = $count_feature_label / $count_label;
+            } else {
+                $conditional_prob[$key] = 0; // Atau tindakan lain sesuai kebutuhan
+            }
         }
 
         // Hitung total probabilitas P(Y=label) * P(X1=x1|Y=label) * P(X2=x2|Y=label) * ...
         $total_prob = $prior;
+
         foreach ($conditional_prob as $value) {
             $total_prob *= $value;
         }
